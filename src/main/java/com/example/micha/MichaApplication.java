3f751cb5dfda6aae5be3d0ff67233fc472ff25da
Micha <micha@Micha02>
2018-08-31 18:29:14 +0200

    Create project

---
+++ src/main/java/com/example/micha/MichaApplication.java
@@ -0,0 +1,12 @@
+package com.example.micha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MichaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MichaApplication.class, args);
+	}
+}
